0e7ea7ba1e4f8ca63f8c24604d8bdc4185d0b5a9
wp <wp15987445474@163.com>
2022-04-15 10:42:28 +0800

    初始化

---
+++ src/Index.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Ry\RyWp;
+
+/**
+ * @className ； Index
+ * @Auth : 麋鹿@ANNIANZU
+ * @Email : 3258581571
+ * @date ；2022/04/15 10:40
+ */
+class Index
+{
+    public function index()
+    {
+        return 'wp';
+    }
+}
---
+++ src/app/Index.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Ry\RyWp\app;
+
+/**
+ * @className ； Index
+ * @Auth : 麋鹿@ANNIANZU
+ * @Email : 3258581571
+ * @date ；2022/04/15 10:41
+ */
+class Index
+{
+    public function __construct()
+    {
+        echo "love to";
+    }
+}
